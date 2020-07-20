fcbf945843178cda190fb9f9ac9945cf7fc9080d
ElijahCoding <cuiyeqing960904@gmail.com>
2020-07-20 23:53:26 +0300

    preparing for templates

--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,42 @@
 from django.shortcuts import render
+from .models import *
 
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = orders.count()
+    total_orders = customers.count()
+
+    delivered = orders.filter(status='Delivered')
+    pending = orders.filter(status='Pending')
+
+    context = {
+        'orders': orders,
+        'customers': customers,
+        'total_customers': total_customers,
+        'total_orders': total_orders,
+        'delivered': delivered,
+        'pending': pending
+    }
+
+    return render(request, 'accounts/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products': products})
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, pk_test):
+    customer = Customer.objects.get(id=pk_test)
+
+    orders = customer.order_set.all()
+    order_count = orders.count()
+
+    return render(request, 'accounts/customer.html', {
+        'customer': customer,
+        'orders': orders,
+        'order_count': order_count
+    })
