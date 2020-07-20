b6b2c47a16a0d52b4a9c52428239f8af73fb1322
ElijahCoding <cuiyeqing960904@gmail.com>
2020-07-20 23:24:32 +0300

    models set up

--- accounts/models.py
+++ accounts/models.py
@@ -10,6 +10,11 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     Category = (
@@ -21,6 +26,10 @@ class Product(models.Model):
     category = models.CharField(max_length=200, null=True, choices=Category)
     description = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     STATUS = (
@@ -28,6 +37,8 @@ class Order(models.Model):
         ('Out for delivery', 'Out for delivery'),
         ('Delivered', 'Delivered'),
     )
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product =  models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
 
