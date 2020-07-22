f166c41d64a60194f5f47870f46d8679b1e39a4b
ElijahCoding <cuiyeqing960904@gmail.com>
2020-07-22 23:22:25 +0300

    login method

--- accounts/views.py
+++ accounts/views.py
@@ -17,7 +17,17 @@ def registerPage(request):
     return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
-    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Username OR Password Incorrect.')
+
     context = {}
     return render(request, 'accounts/login.html', context)
 
