b0b03f36385c6ed6762ced3e98dfcc4560b5cbcd
ElijahCoding <cuiyeqing960904@gmail.com>
2020-07-23 16:26:14 +0300

    auth middleware

--- accounts/views.py
+++ accounts/views.py
@@ -5,32 +5,44 @@ from django.forms import inlineformset_factory
 from .filters import OrderFilter
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def registerPage(request):
-    form = CreateUserForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        user = form.cleaned_data.get('username')
-        messages.success(request, 'Account was created for ' + user)
-        return redirect('login')
-    context = {'form': form}
-    return render(request, 'accounts/register.html', context)
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login')
+        context = {'form': form}
+        return render(request, 'accounts/register.html', context)
 
 def loginPage(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.info(request, 'Username OR Password Incorrect.')
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-    context = {}
-    return render(request, 'accounts/login.html', context)
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Username OR Password Incorrect.')
 
+        context = {}
+        return render(request, 'accounts/login.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+@login_required(login_url='login')
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -52,12 +64,12 @@ def home(request):
 
     return render(request, 'accounts/dashboard.html', context)
 
-
+@login_required(login_url='login')
 def products(request):
     products = Product.objects.all()
     return render(request, 'accounts/products.html', {'products': products})
 
-
+@login_required(login_url='login')
 def customer(request, pk_test):
     customer = Customer.objects.get(id=pk_test)
 
