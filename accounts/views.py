c74d47fe81c7f1ac495f8cc6db84aaf90179e9e5
ElijahCoding <cuiyeqing960904@gmail.com>
2020-07-25 23:11:51 +0300

    account image uploading

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
-from .forms import OrderForm, CreateUserForm
+from .forms import OrderForm, CreateUserForm, CustomerForm
 from django.forms import inlineformset_factory
 from .filters import OrderFilter
 from django.contrib import messages
@@ -89,6 +89,20 @@ def userPage(request):
     }
     return render(request, 'accounts/user.html', context)
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
+def accountSettings(request):
+    form = CustomerForm(instance=request.user)
+
+    if request.method == 'POST':
+        form = CustomerForm(request.POST, request.FILES, instance=request.user)
+        if form.is_valid():
+            form.save()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/account_settings.html', context)
 
 @login_required(login_url='login')
 @allowed_users(allowed_roles=['admin'])
